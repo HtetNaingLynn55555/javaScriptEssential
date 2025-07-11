fad848379ca1bdd094fbf62570bfd0155359beac
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-07-11 06:31:20 -0400

    add reset

--- flowersCount/followers_count.js
+++ flowersCount/followers_count.js
@@ -18,3 +18,9 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
   }
+
+  function resetCount()
+  {
+    count = 0;
+    alert ("now your Instagram post follower count reset into zero")
+  }
